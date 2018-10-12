a0816dc6d0b31338a6a3225ac4946aaab8f41e0a
IMike17 <iovinomic@gmail.com>
2018-10-12 21:09:36 +0200

    Test Configuration, Test Utils & Model, UserTests

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -22,7 +22,17 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     
     let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    
+    let databaseName: String
+    let databasePort: Int
+    if (env == .testing) {
+        databaseName = "vapor-test"
+        databasePort = 5433
+    } else {
+        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+        databasePort = 5432
+    }
+    
     let password = Environment.get("DATABASE_PASSWORD") ?? "password"
     
     let databaseConfig: PostgreSQLDatabaseConfig
@@ -32,6 +42,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     } else {
         databaseConfig = PostgreSQLDatabaseConfig(
             hostname: hostname,
+            port: databasePort,
             username: username,
             database: databaseName,
             password: password)
