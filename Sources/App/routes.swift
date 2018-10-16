ebfbb22edbc45e1fa8645ea54dad0d58442fd806
IMike17 <iovinomic@gmail.com>
2018-10-16 19:12:15 +0200

    Configuring Leaf Hello World

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,10 +7,12 @@ public func routes(_ router: Router) throws {
     let acronymsController = AcronymsController()
     let usersController = UsersController()
     let categoriesController = CategoriesController()
+	let webSiteController = WebsiteController()
     
     try router.register(collection: acronymsController)
     try router.register(collection: usersController)
     try router.register(collection: categoriesController)
+	try router.register(collection: webSiteController)
     
     // Basic "Hello, world!" example
     router.get("hello") { req in
