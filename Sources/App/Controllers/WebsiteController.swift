251713d7218de9eb1355d594e7cb9664984d5d8a
IMike17 <iovinomic@gmail.com>
2018-10-17 17:32:10 +0200

    Mark Comments & Ordering

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -3,29 +3,22 @@ import Leaf
 
 struct WebsiteController: RouteCollection {
 	func boot(router: Router) throws {
-		router.get(use: indexHandler)
 		router.get("acronyms", Acronym.parameter, use: self.acronymHandler)
-		router.get("users", User.parameter, use: self.userHandler)
-		router.get("users", use: self.allUsersHandler)
-		router.get("categories", use: self.allCategoriesHandler)
-		router.get("categories", Category.parameter, use: self.categoryHandler)
 		router.get("acronyms", "create", use: self.createAcronymHandler)
 		router.post(Acronym.self, at: "acronyms", "create", use: self.createAcronymPostHandler)
+		router.post("acronyms", Acronym.parameter, "delete", use: self.deleteAcronymHandler)
 		router.get("acronyms", Acronym.parameter, "edit", use: self.editAcronymHandler)
 		router.post("acronyms", Acronym.parameter, "edit", use: self.editAcronymPostHandler)
-		router.post("acronyms", Acronym.parameter, "delete", use: self.deleteAcronymHandler)
+		router.get("categories", use: self.allCategoriesHandler)
+		router.get("categories", Category.parameter, use: self.categoryHandler)
+		router.get(use: indexHandler)
+		router.get("users", use: self.allUsersHandler)
+		router.get("users", User.parameter, use: self.userHandler)
 	}
 	
-	func indexHandler(_ req: Request) throws -> Future<View> {
-		return Acronym.query(on: req)
-			.all()
-			.flatMap(to: View.self, { acronyms in
-				let acronymsData = acronyms.isEmpty ? nil : acronyms
-				let context = IndexContext(title: "Homepage", acronyms: acronymsData)
-				return try req.view().render("index", context)
-			})
-	}
+	// MARK: - Handlers
 	
+	// MARK: - Acronym Handlers
 	func acronymHandler(_ req: Request) throws -> Future<View> {
 		return try req.parameters.next(Acronym.self)
 			.flatMap(to: View.self, { acronym in
@@ -39,57 +32,6 @@ struct WebsiteController: RouteCollection {
 						
 						return try req.view().render("acronym", context)
 					})
-			})
-	}
-	
-	func userHandler(_ req: Request) throws -> Future<View> {
-		return try req.parameters.next(User.self)
-			.flatMap(to: View.self, { user in
-				return try user.acronyms
-					.query(on: req)
-					.all()
-					.flatMap(to: View.self, { acronyms in
-						let contex = UserContext(
-							title: user.name,
-							user: user,
-							acronyms: acronyms)
-						
-						return try req.view().render("user", contex)
-					})
-			})
-	}
-	
-	func allUsersHandler(_ req: Request) throws -> Future<View> {
-		return User.query(on: req)
-			.all()
-			.flatMap(to: View.self, { users in
-				let context = AllUsersContext(
-					title: "All Users",
-					users: users)
-				
-				return try req.view().render("allUsers", context)
-			})
-	}
-	
-	func allCategoriesHandler(_ req: Request) throws -> Future<View> {
-		let categories = Category.query(on: req).all()
-		let context = AllCategoriesContext(categories: categories)
-		
-		return try req.view().render("allCategories", context)
-	}
-	
-	func categoryHandler(_ req: Request) throws -> Future<View> {
-		return try req.parameters.next(Category.self)
-			.flatMap(to: View.self, { category in
-				let acronyms = try category.acronyms
-					.query(on: req)
-					.all()
-				let context = CategoryContext(
-					title: category.name,
-					category: category,
-					acronyms: acronyms)
-				
-				return try req.view().render("category", context)
 			})
 	}
 	
@@ -109,6 +51,12 @@ struct WebsiteController: RouteCollection {
 				
 				return req.redirect(to: "/acronyms/\(id)")
 			})
+	}
+	
+	func deleteAcronymHandler(_ req: Request) throws -> Future<Response> {
+		return try req.parameters.next(Acronym.self)
+			.delete(on: req)
+			.transform(to: req.redirect(to: "/"))
 	}
 	
 	func editAcronymHandler(_ req: Request) throws -> Future<View> {
@@ -144,33 +92,78 @@ struct WebsiteController: RouteCollection {
 		})
 	}
 	
-	func deleteAcronymHandler(_ req: Request) throws -> Future<Response> {
-		return try req.parameters.next(Acronym.self)
-			.delete(on: req)
-			.transform(to: req.redirect(to: "/"))
+	// MARK: - Category Handlers
+	func allCategoriesHandler(_ req: Request) throws -> Future<View> {
+		let categories = Category.query(on: req).all()
+		let context = AllCategoriesContext(categories: categories)
+		
+		return try req.view().render("allCategories", context)
+	}
+	
+	func categoryHandler(_ req: Request) throws -> Future<View> {
+		return try req.parameters.next(Category.self)
+			.flatMap(to: View.self, { category in
+				let acronyms = try category.acronyms
+					.query(on: req)
+					.all()
+				let context = CategoryContext(
+					title: category.name,
+					category: category,
+					acronyms: acronyms)
+				
+				return try req.view().render("category", context)
+			})
+	}
+	
+	// MARK: - Index Handlers
+	func indexHandler(_ req: Request) throws -> Future<View> {
+		return Acronym.query(on: req)
+			.all()
+			.flatMap(to: View.self, { acronyms in
+				let acronymsData = acronyms.isEmpty ? nil : acronyms
+				let context = IndexContext(title: "Homepage", acronyms: acronymsData)
+				return try req.view().render("index", context)
+			})
+	}
+	
+	// MARK: - User Handlers
+	func allUsersHandler(_ req: Request) throws -> Future<View> {
+		return User.query(on: req)
+			.all()
+			.flatMap(to: View.self, { users in
+				let context = AllUsersContext(
+					title: "All Users",
+					users: users)
+				
+				return try req.view().render("allUsers", context)
+			})
+	}
+	
+	func userHandler(_ req: Request) throws -> Future<View> {
+		return try req.parameters.next(User.self)
+			.flatMap(to: View.self, { user in
+				return try user.acronyms
+					.query(on: req)
+					.all()
+					.flatMap(to: View.self, { acronyms in
+						let contex = UserContext(
+							title: user.name,
+							user: user,
+							acronyms: acronyms)
+						
+						return try req.view().render("user", contex)
+					})
+			})
 	}
 }
 
+// MARK: - Contexts
+
+// MARK: - Acronym
 struct AcronymContext: Encodable {
 	let title: String
 	let acronym: Acronym
 	let user: User
-}
-
-struct AllCategoriesContext: Encodable {
-	let title = "All Categories"
-	let categories: Future<[Category]>
-}
-
-struct AllUsersContext: Encodable {
-	let title: String
-	let users: [User]
-}
-
-struct CategoryContext: Encodable {
-	let title: String
-	let category: Category
-	let acronyms: Future<[Acronym]>
 }
 
 struct CreateAcronymContext: Encodable {
@@ -185,9 +178,28 @@ struct EditAcronymContext: Encodable {
 	let editing = true
 }
 
+// MARK: - Category
+struct AllCategoriesContext: Encodable {
+	let title = "All Categories"
+	let categories: Future<[Category]>
+}
+
+struct CategoryContext: Encodable {
+	let title: String
+	let category: Category
+	let acronyms: Future<[Acronym]>
+}
+
+// MARK: - Index
 struct IndexContext: Encodable {
 	let title: String
 	let acronyms: [Acronym]?
+}
+
+// MARK: - User
+struct AllUsersContext: Encodable {
+	let title: String
+	let users: [User]
 }
 
 struct UserContext: Encodable {
