425b9ec92dab77a453592d33b47f6cc8d7d9b41a
IMike17 <iovinomic@gmail.com>
2018-11-07 13:06:54 +0100

    Imperial for OAuth Providers - Google Login

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,10 +8,12 @@ public func routes(_ router: Router) throws {
     let usersController = UsersController()
     let categoriesController = CategoriesController()
 	let webSiteController = WebsiteController()
+	let imperialController = ImperialController()
     
     try router.register(collection: acronymsController)
     try router.register(collection: usersController)
     try router.register(collection: categoriesController)
 	try router.register(collection: webSiteController)
+	try router.register(collection: imperialController)
     
 }
